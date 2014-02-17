0ca5f6f3c73de1b00be28c467b5667971b928a60
Doug Goss <Doug@Dougs-MacBook-Pro.local>
2014-02-16 21:19:49 -0500

    Initial Commit

---
+++ homework3/Homepwner2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Homepwner2AppDelegate.h
+//  homework3
+//
+//  Created by Doug Goss on 2/16/14.
+//  Copyright (c) 2014 Doug Goss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Homepwner2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ homework3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  homework3
+//
+//  Created by Doug Goss on 2/16/14.
+//  Copyright (c) 2014 Doug Goss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Homepwner2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Homepwner2AppDelegate class]));
+    }
+}
